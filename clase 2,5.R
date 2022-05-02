0214e0f4b9b077c3b4a5fedd46459d3604d89d9b
WalterVN13 <103954491+WalterVN13@users.noreply.github.com>
2022-05-02 12:28:54 -0600

    Delete clase 2,5.R

--- clase 2,5.R
+++
@@ -1,5 +0,0 @@
-covid <-
-  read.csv("https://raw.githubusercontent.com/gf0604-procesamientodatosgeograficos/2022-i/main/datos/cepredenac/covid/covid-centroamericard-20210422.csv")
-
-covid_CostaRica_positivos <-
-  read.csv("C:/Users/gf0604-1/prsctics-gdsl/datso covid/04_26_22_CSV_POSITIVOS.csv", sep = ";")
